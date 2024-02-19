1ad2f1f01dc353170576a36843cc9dda5c1e1e36
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-02-19 20:18:48 +0200

    update

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const btnOne = document.getElementById('btnOne');
+const searchBtn = document.getElementById('searchBtn');
+const inputWord = document.getElementById('search');
+// const items = document.querySelectorAll('.card');
+const back = document.querySelector('.list');
+const items = [
+    'Treasure',
+    'Agatha',
+    'Christopher',
+    'Ahijah'
+]
+
+searchBtn.addEventListener('click', function() {
+if (inputWord.value === '') {
+    alert('You never searched for anything!!')
+} else {
+    alert('you searched for '+inputWord.value+'.')
+    inputWord.value = '';
+}
+})
+
+
+
+function section() {
+    const btnOne = document.getElementById('btnOne');
 const btnTwo = document.getElementById('btnTwo');
 const musicBtn = document.getElementById('music');
 const removeBtn = document.getElementById('remove');
-const player = document.querySelector('h3');
+const player = document.querySelector('.player');
+
 
 const sectionOne = document.querySelector('.sectionOne');
 const sectionTwo = document.querySelector('.sectionTwo');
@@ -28,3 +52,7 @@ btnTwo.addEventListener('click', function(e){
     player.style = 'display: none;';
      
  });
+
+}
+
+section();
